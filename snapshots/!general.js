d236a5b6e50cbbe8a248c3c29c6e6a460ee82f04
Alexander Lyon <arlyon@me.com>
2022-09-23 11:35:46 +0100

    refactor: replace quotes to match swc

--- snapshots/!general.js
+++ snapshots/!general.js
@@ -16,64 +16,64 @@ const MediaPropertyDuplicates = tw`lg:bg-blue-500 lg:bg-black`
 
 // Only basic evaluations supported
 // No functions or "beyond basic" conditionals.
-const plainConditional = true && 'red'
+const plainConditional = true && "red"
 const plainVariable = `bg-${plainConditional}-500`
 tw`${plainVariable}`
 
       ↓ ↓ ↓ ↓ ↓ ↓
 
-import _styled from '@emotion/styled'
+import _styled from "@emotion/styled"
 
 /**
  * Misc usage tests
  */
 const styles = {
-  textTransform: 'uppercase',
+  textTransform: "uppercase",
 }
 
 const Box = _styled.div({
-  '--tw-text-opacity': '1',
-  color: 'rgb(239 68 68 / var(--tw-text-opacity))',
+  "--tw-text-opacity": "1",
+  color: "rgb(239 68 68 / var(--tw-text-opacity))",
 })
 
 const BoxExtended = _styled(Box)({
-  '--tw-bg-opacity': '1',
-  backgroundColor: 'rgb(59 130 246 / var(--tw-bg-opacity))',
+  "--tw-bg-opacity": "1",
+  backgroundColor: "rgb(59 130 246 / var(--tw-bg-opacity))",
 }) // Media queries
 
 const MediaProperty = {
-  '@media (min-width: 1024px)': {
-    textTransform: 'uppercase',
+  "@media (min-width: 1024px)": {
+    textTransform: "uppercase",
   },
 }
 
 const MediaColorProperty = _styled.div({
-  '@media (min-width: 1024px)': {
-    '--tw-text-opacity': '1',
-    color: 'rgb(239 68 68 / var(--tw-text-opacity))',
+  "@media (min-width: 1024px)": {
+    "--tw-text-opacity": "1",
+    color: "rgb(239 68 68 / var(--tw-text-opacity))",
   },
 })
 
 const ElementMediaColorProperty = _styled(Box)({
-  '@media (min-width: 1024px)': {
-    '--tw-bg-opacity': '1',
-    backgroundColor: 'rgb(59 130 246 / var(--tw-bg-opacity))',
+  "@media (min-width: 1024px)": {
+    "--tw-bg-opacity": "1",
+    backgroundColor: "rgb(59 130 246 / var(--tw-bg-opacity))",
   },
 })
 
 const MediaPropertyDuplicates = {
-  '@media (min-width: 1024px)': {
-    '--tw-bg-opacity': '1',
-    backgroundColor: 'rgb(0 0 0 / var(--tw-bg-opacity))',
+  "@media (min-width: 1024px)": {
+    "--tw-bg-opacity": "1",
+    backgroundColor: "rgb(0 0 0 / var(--tw-bg-opacity))",
   },
 } // Only basic evaluations supported
 // No functions or "beyond basic" conditionals.
 
-const plainConditional = true && 'red'
+const plainConditional = true && "red"
 const plainVariable = `bg-${plainConditional}-500`
 ({
-  '--tw-bg-opacity': '1',
-  backgroundColor: 'rgb(239 68 68 / var(--tw-bg-opacity))',
+  "--tw-bg-opacity": "1",
+  backgroundColor: "rgb(239 68 68 / var(--tw-bg-opacity))",
 })
 
 
