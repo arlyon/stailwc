fbb49d2352d1babad1d504a4c532413d73591a0f
Alexander Lyon <arlyon@me.com>
2022-11-02 18:18:21 +0000

    refactor: support transparent and regular subcommands

--- crates/tailwind-parse-macro/src/lib.rs
+++ crates/tailwind-parse-macro/src/lib.rs
@@ -5,17 +5,34 @@ use std::cmp::max;
 use convert_case::{Case, Casing};
 use proc_macro::TokenStream;
 use quote::quote;
-use syn::{parse_macro_input, ItemEnum};
+use syn::{parse_macro_input, Ident, Item, ItemEnum, ItemMod, Variant};
 
 #[proc_macro_attribute]
 pub fn parser(_attr: TokenStream, input: TokenStream) -> TokenStream {
-    let mut variants = parse_macro_input!(input as ItemEnum);
-    let name = variants.ident.clone();
+    let mut module = parse_macro_input!(input as ItemMod);
+    let (_, content) = module.content.as_mut().expect("module must have a root");
+
+    let mut root = content
+        .drain_filter(|i| {
+            if let Item::Enum(i) = i {
+                i.attrs
+                    .drain_filter(|a| a.path.segments.last().unwrap().ident.to_string().eq("root"))
+                    .count()
+                    > 0
+            } else {
+                false
+            }
+        })
+        .filter_map(|i| if let Item::Enum(e) = i { Some(e) } else { None })
+        .next()
+        .expect("module must have a root");
+
+    let name = root.ident.clone();
 
     let mut max_dashes = 0;
     let mut subcommands = vec![];
 
-    let x = variants
+    let x = root
         .variants
         .iter_mut()
         .map(|v| {
@@ -23,64 +40,155 @@ pub fn parser(_attr: TokenStream, input: TokenStream) -> TokenStream {
             let kebab = v.ident.to_string().to_case(Case::Kebab);
 
             if let Some(f) = v.fields.iter().next() {
-                if let syn::Type::Path(p) = &f.ty {
-                    let transparent = v
-                        .attrs
-                        .drain_filter(|a| a.path.get_ident().unwrap() == "transparent")
-                        .count()
-                        > 0;
+                let p = match &f.ty {
+                    syn::Type::Path(p) => p,
+                    _ => panic!("subcommand must be a struct"),
+                };
 
-                    let type_name = p.path.segments.last().map(|s| s.ident.to_string());
+                let optional = extract_type_from_option(&p.path);
 
-                    // todo: determine method to get these subcommands
+                // find our target type, either from the option or the path
+                let target = optional
+                    .and_then(|o| match &o {
+                        syn::Type::Path(p) => Some(&p.path),
+                        _ => None,
+                    })
+                    .unwrap_or(&p.path)
+                    .segments
+                    .last()
+                    .map(|s| &s.ident);
 
-                    return if transparent {
-                        // transparent commands should be placed directly in the hash table
-                        quote! {}
-                    } else {
-                        // other commands should be handled seperately
-                        subcommands.push(ident.clone());
-                        if type_name.as_deref() != Some("Option") {
-                            println!("UNSUPPORTED SUBCOMMAND {}", ident);
-                            quote! {}
-                        } else {
-                            quote! {#kebab => #name::#ident(None),}
+                // locate the type name, and subcommand enum
+                let subcommand = target
+                    .and_then(|ident| get_enum_by_ident(content, ident))
+                    .expect("enum variant must have a type");
+
+                let format = if v
+                    .attrs
+                    .drain_filter(|a| a.path.get_ident().unwrap() == "transparent")
+                    .count()
+                    > 0
+                {
+                    subcommands.push(ident.clone());
+                    fmt_transparent
+                } else {
+                    fmt_regular
+                };
+
+                let subcommands = subcommand.variants.iter().map(|v| {
+                    let kebab = format(v, &kebab);
+                    let sub_name = subcommand.ident.clone();
+                    let sub_ident = v.ident.clone();
+                    if optional.is_some() {
+                        quote! {
+                            #kebab => #name::#ident(Some(#sub_name::#sub_ident)),
                         }
-                    };
-                }
-            }
+                    } else {
+                        quote! {
+                            #kebab => #name::#ident(#sub_name::#sub_ident),
+                        }
+                    }
+                });
 
-            max_dashes = max(max_dashes, kebab.chars().filter(|&c| c == '-').count());
-            quote! {#kebab => #name::#ident,}
+                if optional.is_some() {
+                    let subcommands = subcommands.chain(std::iter::once(quote! {
+                        #kebab => #name::#ident(None),
+                    }));
+                    quote! {
+                        #(#subcommands)*
+                    }
+                } else {
+                    quote! {
+                        #(#subcommands)*
+                    }
+                }
+            } else {
+                max_dashes = max(max_dashes, kebab.chars().filter(|&c| c == '-').count());
+                quote! {#kebab => #name::#ident,}
+            }
         })
         .collect::<Vec<_>>();
 
     TokenStream::from(quote! {
-        #variants
+        mod plugin {
+            #root
 
-        impl std::str::FromStr for #name {
-            type Err = ();
+            #(#content)*
 
-            fn from_str(s: &str) -> Result<Self, Self::Err> {
-                PLUGINS.get(s).copied().ok_or(())
-            }
-        }
+            impl std::str::FromStr for #name {
+                type Err = ();
 
-        impl #name {
-            pub const fn max_dashes() -> usize {
-                #max_dashes
-            }
-
-            pub fn has_subcommand(&self) -> bool {
-                match self {
-                    #(#name::#subcommands(_))|* => true,
-                    _ => false
+                fn from_str(s: &str) -> Result<Self, Self::Err> {
+                    PLUGINS.get(s).copied().ok_or(())
                 }
             }
-        }
 
-        static PLUGINS: phf::Map<&'static str, #name> = phf::phf_map! {
-            #(#x)*
-        };
+            impl #name {
+                pub const fn max_dashes() -> usize {
+                    #max_dashes
+                }
+
+                pub fn has_subcommand(&self) -> bool {
+                    match self {
+                        #(#name::#subcommands(_))|* => true,
+                        _ => false
+                    }
+                }
+            }
+
+            static PLUGINS: phf::Map<&'static str, #name> = phf::phf_map! {
+                #(#x)*
+            };
+        }
     })
+}
+
+fn get_enum_by_ident<'a>(content: &'a [Item], ident: &Ident) -> Option<&'a ItemEnum> {
+    content
+        .iter()
+        .filter_map(|i| if let Item::Enum(e) = i { Some(e) } else { None })
+        .find(|e| e.ident.eq(ident))
+}
+
+fn extract_type_from_option(path: &syn::Path) -> Option<&syn::Type> {
+    use syn::{GenericArgument, Path, PathArguments, PathSegment};
+
+    // TODO store (with lazy static) the vec of string
+    // TODO maybe optimization, reverse the order of segments
+    fn extract_option_segment(path: &Path) -> Option<&PathSegment> {
+        let idents_of_path = path
+            .segments
+            .iter()
+            .into_iter()
+            .fold(String::new(), |mut acc, v| {
+                acc.push_str(&v.ident.to_string());
+                acc.push('|');
+                acc
+            });
+        ["Option|", "std|option|Option|", "core|option|Option|"]
+            .into_iter()
+            .find(|s| idents_of_path == *s)
+            .and_then(|_| path.segments.last())
+    }
+    extract_option_segment(path)
+        .and_then(|path_seg| {
+            let type_params = &path_seg.arguments;
+            // It should have only on angle-bracketed param ("<String>"):
+            match *type_params {
+                PathArguments::AngleBracketed(ref params) => params.args.first(),
+                _ => None,
+            }
+        })
+        .and_then(|generic_arg| match *generic_arg {
+            GenericArgument::Type(ref ty) => Some(ty),
+            _ => None,
+        })
+}
+
+fn fmt_transparent(v: &Variant, _kebab: &str) -> String {
+    v.ident.to_string().to_case(Case::Kebab)
+}
+
+fn fmt_regular(v: &Variant, kebab: &str) -> String {
+    format!("{kebab}-{}", v.ident.to_string().to_case(Case::Kebab))
 }
--- crates/tailwind-parse/src/lib.rs
+++ crates/tailwind-parse/src/lib.rs
@@ -1,198 +1,204 @@
-use nom::{bytes::complete::take_while, combinator::map_res, IResult};
-use nom_locate::LocatedSpan;
-use swc_core::common::Span;
-
-type NomSpan<'a> = LocatedSpan<&'a str, Span>;
+pub use plugin::*;
 
 #[tailwind_parse_macro::parser]
-#[derive(Copy, Clone, Eq, PartialEq, Debug)]
-pub enum Plugin {
-    #[transparent]
-    Position(Position),
-    #[transparent]
-    Visibility(Visibility),
-    #[transparent]
-    Display(Display),
-    #[transparent]
-    TextTransform(TextTransform),
-    #[transparent]
-    TextDecoration(TextDecoration),
+mod plugin {
+    use nom::{bytes::complete::take_while, combinator::map_res, IResult};
+    use nom_locate::LocatedSpan;
+    use swc_core::common::Span;
 
-    Border(Option<Border>),
-    H,
-    W,
-    Min(Min),
-    Max(Max),
-    P,
-    Px,
-    Pl,
-    Pr,
-    Py,
-    Pt,
-    Pb,
-    M,
-    Mx,
-    My,
-    Ml,
-    Mr,
-    Mt,
-    Mb,
-    Z,
-    Text,
-    Bg,
-    Font,
-    Shadow,
-    Transition,
-    Placeholder,
-    Delay,
-    Duration,
-    Divide,
-    Rotate,
-    Appearance,
-    Pointer,
-    Ease,
-    Order,
-    Rounded(Option<Rounded>),
-    From,
-    To,
-    Outline,
-    Mix,
-    Flex,
-    Grid,
-    Col,
-    Grow,
-    Shrink,
-    Basis,
-    Justify,
-    Items,
-    Gap,
-    Cursor,
-    Scale,
-    Box,
-    Select,
-    Overflow,
-    Top,
-    Bottom,
-    Left,
-    Right,
-    Translate,
-    Tracking,
-    Invert,
-    Space,
-    Transform,
-    Opacity,
-    Blur,
-    Ring,
-    Sr,
-}
+    type NomSpan<'a> = LocatedSpan<&'a str, Span>;
 
-#[derive(Copy, Clone, Eq, PartialEq, Debug)]
-pub enum Position {
-    Static,
-    Fixed,
-    Absolute,
-    Relative,
-    Sticky,
-}
+    #[derive(Copy, Clone, Eq, PartialEq, Debug)]
+    #[root]
+    pub enum Plugin {
+        #[transparent]
+        Position(Position),
+        #[transparent]
+        Visibility(Visibility),
+        #[transparent]
+        Display(Display),
+        #[transparent]
+        TextTransform(TextTransform),
+        #[transparent]
+        TextDecoration(TextDecoration),
 
-#[derive(Copy, Clone, Eq, PartialEq, Debug)]
-pub enum Visibility {
-    Visible,
-    Invisible,
-}
+        Border(Option<Border>),
+        H,
+        W,
+        Min(Min),
+        Max(Max),
+        P,
+        Px,
+        Pl,
+        Pr,
+        Py,
+        Pt,
+        Pb,
+        M,
+        Mx,
+        My,
+        Ml,
+        Mr,
+        Mt,
+        Mb,
+        Z,
+        Text,
+        Bg,
+        Font,
+        Shadow,
+        Transition,
+        Placeholder,
+        Delay,
+        Duration,
+        Divide,
+        Rotate,
+        Appearance,
+        Pointer,
+        Ease,
+        Order,
+        Rounded(Option<Rounded>),
+        From,
+        To,
+        Outline,
+        Mix,
+        Flex,
+        Grid,
+        Col,
+        Grow,
+        Shrink,
+        Basis,
+        Justify,
+        Items,
+        Gap,
+        Cursor,
+        Scale,
+        Box,
+        Select,
+        Overflow,
+        Top,
+        Bottom,
+        Left,
+        Right,
+        Translate,
+        Tracking,
+        Invert,
+        Space,
+        Transform,
+        Opacity,
+        Blur,
+        Ring,
+        Sr,
+    }
 
-#[derive(Copy, Clone, Eq, PartialEq, Debug)]
-pub enum Display {
-    Block,
-    InlineBlock,
-    Inline,
-    Flex,
-    InlineFlex,
-    Table,
-    InlineTable,
-    TableCaption,
-    TableCell,
-    TableColumn,
-    TableColumnGroup,
-    TableFooterGroup,
-    TableHeaderGroup,
-    TableRowGroup,
-    TableRow,
-    FlowRoot,
-    Grid,
-    InlineGrid,
-    Contents,
-    ListItem,
-    Hidden,
-}
+    #[derive(Copy, Clone, Eq, PartialEq, Debug)]
+    pub enum Position {
+        Static,
+        Fixed,
+        Absolute,
+        Relative,
+        Sticky,
+    }
 
-#[derive(Copy, Clone, Eq, PartialEq, Debug)]
-pub enum TextTransform {
-    Uppercase,
-    Lowercase,
-    Capitalize,
-    NormalCase,
-}
+    #[derive(Copy, Clone, Eq, PartialEq, Debug)]
+    pub enum Visibility {
+        Visible,
+        Invisible,
+    }
 
-#[derive(Copy, Clone, Eq, PartialEq, Debug)]
-pub enum TextDecoration {
-    Underline,
-    Overline,
-    LineThrough,
-    NoUnderline,
-}
+    /// The options for the `display` property.
+    ///
+    /// Note: flex and grid are handled by their own plugins.
+    #[derive(Copy, Clone, Eq, PartialEq, Debug)]
+    pub enum Display {
+        Block,
+        InlineBlock,
+        Inline,
+        InlineFlex,
+        Table,
+        InlineTable,
+        TableCaption,
+        TableCell,
+        TableColumn,
+        TableColumnGroup,
+        TableFooterGroup,
+        TableHeaderGroup,
+        TableRowGroup,
+        TableRow,
+        FlowRoot,
+        InlineGrid,
+        Contents,
+        ListItem,
+        Hidden,
+    }
 
-#[derive(Copy, Clone, Eq, PartialEq, Debug)]
-pub enum Rounded {
-    T,
-    B,
-    L,
-    R,
-    Tl,
-    Tr,
-    Bl,
-    Br,
-}
-#[derive(Copy, Clone, Eq, PartialEq, Debug)]
-pub enum Border {
-    B,
-    T,
-    L,
-    R,
-    X,
-    Y,
-}
-#[derive(Copy, Clone, Eq, PartialEq, Debug)]
-pub enum Min {
-    W,
-    H,
-}
-#[derive(Copy, Clone, Eq, PartialEq, Debug)]
-pub enum Max {
-    W,
-    H,
-}
+    #[derive(Copy, Clone, Eq, PartialEq, Debug)]
+    pub enum TextTransform {
+        Uppercase,
+        Lowercase,
+        Capitalize,
+        NormalCase,
+    }
 
-impl<'a> Plugin {
-    pub fn parse(s: NomSpan<'a>) -> IResult<NomSpan<'a>, Self, nom::error::Error<NomSpan<'a>>> {
-        let max = Plugin::max_dashes();
+    #[derive(Copy, Clone, Eq, PartialEq, Debug)]
+    pub enum TextDecoration {
+        Underline,
+        Overline,
+        LineThrough,
+        NoUnderline,
+    }
 
-        let cmd = map_res(take_while(|c| c != '-'), |s: NomSpan<'a>| {
-            s.parse::<Plugin>()
-        })(s);
+    #[derive(Copy, Clone, Eq, PartialEq, Debug)]
+    pub enum Rounded {
+        T,
+        B,
+        L,
+        R,
+        Tl,
+        Tr,
+        Bl,
+        Br,
+    }
+    #[derive(Copy, Clone, Eq, PartialEq, Debug)]
+    pub enum Border {
+        B,
+        T,
+        L,
+        R,
+        X,
+        Y,
+    }
+    #[derive(Copy, Clone, Eq, PartialEq, Debug)]
+    pub enum Min {
+        W,
+        H,
+    }
+    #[derive(Copy, Clone, Eq, PartialEq, Debug)]
+    pub enum Max {
+        W,
+        H,
+    }
 
-        // if the command has sub-commands, attempt to parse
-        // if that fails, and the subcommand is optional, keep it,
-        // otherwise return an error
+    impl<'a> Plugin {
+        pub fn parse(s: NomSpan<'a>) -> IResult<NomSpan<'a>, Self, nom::error::Error<NomSpan<'a>>> {
+            let _max = Plugin::max_dashes();
 
-        if cmd
-            .as_ref()
-            .map(|(s, c)| c.has_subcommand())
-            .unwrap_or(false)
-        {
-            println!("NOT SUPPORTED");
+            let cmd = map_res(take_while(|c| c != '-'), |s: NomSpan<'a>| {
+                s.parse::<Plugin>()
+            })(s);
+
+            // if the command has sub-commands, attempt to parse
+            // if that fails, and the subcommand is optional, keep it,
+            // otherwise return an error
+
+            if cmd
+                .as_ref()
+                .map(|(_, c)| c.has_subcommand())
+                .unwrap_or(false)
+            {
+                println!("NOT SUPPORTED");
+            }
+
+            cmd
         }
-
-        cmd
     }
 }
